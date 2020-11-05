c71b939d91319a7dab478b1ed0d4245034ed314a
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2020-11-05 15:31:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
-
 dotenv.config();
 
 const app = express();
@@ -26,9 +25,10 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    console.log('fetch request data', data);
   });
 
 app.listen(port, () => {
