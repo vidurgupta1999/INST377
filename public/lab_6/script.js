dde866eb85baa2211480df62046177ef0b808051
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2020-11-06 14:14:37 -0500

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,7 +1,7 @@
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
 function range(int) {
@@ -15,43 +15,46 @@ function range(int) {
 function sortFunction(a, b, key) {
   if (a[key] < b[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  }
+  if (a[key] > b[key]) {
     return 1;
   }
   return 0;
 }
 
-document.body.addEventListener('submit', async (e) => {
+document.body.addEventListener("submit", async (e) => {
   e.preventDefault();
   const form = $(e.target).serializeArray();
-  fetch('/api', {
-    method: 'POST',
+  fetch("/api", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(form)
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(form),
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      if (document.querySelector('.flex-inner')) {
-        document.querySelector('.flex-inner').remove();
+      if (document.querySelector(".flex-inner")) {
+        document.querySelector(".flex-inner").remove();
       }
       const newArr = range(10);
       const newArr2 = newArr.map(() => {
         const number = getRandomIntInclusive(0, 243);
         return fromServer[number];
       });
-    
-      const reverseList = newArr2.sort((a, b) => sortByKey(b, a, 'name'));
-      const ul = docuemnt.createElement('ul');
-      ul.className = 'flex-inner';
-      $('form').prepend(ul);
-      reverseList.forEach((el, i)  => {
-        const li = document.createElement('li');
-        $(li).append(`<input type="checkbox" value = ${el.code} id=${el.code}/> `);
+
+      const reverseList = newArr2.sort((a, b) => sortByKey(b, a, "name"));
+      const ul = docuemnt.createElement("ul");
+      ul.className = "flex-inner";
+      $("form").prepend(ul);
+      reverseList.forEach((el, i) => {
+        const li = document.createElement("li");
+        $(li).append(
+          `<input type="checkbox" value = ${el.code} id=${el.code}/> `
+        );
         $(li).append(`<label for = ${el.code}>${el.name} </label>`);
         $(ul).append(li);
       });
     })
     .catch((err) => console.log(err));
-  });
+});
