6213c7b6637449de714d43eb3fb4943444581739
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2020-11-06 14:31:54 -0500

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,6 +1,6 @@
 function getRandomIntInclusive(min, max) {
-  const min = Math.ceil(min);
-  const max = Math.floor(max);
+   min = Math.ceil(min);
+   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
