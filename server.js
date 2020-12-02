5853695424f44cbcab918ec60db72b28ca44b05e
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2020-12-01 19:12:00 -0500

    Changes

--- server.js
+++ server.js
@@ -4,12 +4,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
 //import countries from "./public/lab_6/countries.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const dbSettings = {
+  filename: './tmp/database.db',s
+  driver: sqlite3.Database
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -42,6 +49,37 @@ app
     res.json(jsondata)
   });
 
-app.listen(port, () => {
+  async function dataFetch() {
+    const url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+    const response = await fetch(url);
+  
+    return response.json()
+  
+  }
+  
+  async function databaseInitialize(dbSettings) {
+    try {
+      const db = await open(dbSettings);
+      await db.exec(`CREATE TABLE IF NOT EXISTS restaurants (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        restaurant_name TEXT,
+        category TEXT)
+        `)
+  
+      const data = await dataFetch();
+  
+      const test = await db.get("SELECT * FROM restaurants")
+      console.log(test);
+  
+    }
+    catch(e) {
+      console.log("Error loading Database");
+      console.log(e);
+  
+    }
+  }
+  
   console.log(`Example app listening on port ${port}!`);
 });
+
+
